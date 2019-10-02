cb6701cee9e032f00ddeff3ef6b1148e0ad163ab
oknaveen <oknaveen9395@gmail.com>
2019-10-02 17:31:18 -0400

    setting up the backend for job portal

---
+++ JobPortal/Routes/jobPortalRoute.js
@@ -0,0 +1,9 @@
+
+//routes
+app.get('/',(req,res)=>{
+    res.send("home page with the input boxes");
+});
+
+app.post('/jobDetails',(req,res)=>{
+    res.send("home page with the input boxes");
+});
